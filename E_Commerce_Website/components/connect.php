479065e8e474a3985cc7150983377d17f663eb87
Al Ifran <ifran15-2915@diu.edu.bd>
2024-09-03 17:10:43 +0600

    updated

--- E_Commerce_Website/components/connect.php
+++ E_Commerce_Website/components/connect.php
@@ -6,13 +6,4 @@ $user_password = '';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
-try {
-  $conn = new PDO($db_name, $user_name, $user_password);
-  // Set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
 ?>
